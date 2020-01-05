818f735036bee2dc68120d28a772308e3e630283
John <bramleyjl@gmail.com>
2020-01-04 23:01:17 -0500

    max_subarray solution

---
+++ curated_75/array/max_subarray.js
@@ -0,0 +1,12 @@
+var maxSubArray = function(array) {
+  count = array[0];
+  max = array[0];
+  for (var i = 1; i < array.length; i++) {
+    count = (count + array[i] > array[i]) ? count + array[i] : array[i];
+    if (count > max) max = count;
+  }
+  return max;
+}
+
+let arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+console.log(maxSubArray(arr));
